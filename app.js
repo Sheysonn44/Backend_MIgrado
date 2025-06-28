ccd4bf3c80024c5a3fb3768b7460acf8d0a01fb6
Sheysonn44 <Sheysonn44@gmail.com>
2025-06-28 09:24:57 -0600

    Cors

--- app.js
+++ app.js
@@ -8,19 +8,25 @@ app.use(express.urlencoded({extended:true}))
 const login = require("./src/routes/loginrouters")
 const teacher = require("./src/routes/teacher.routes")
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175'];
+//const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175'];
+const allowedOrigins = [
+  "https://proyecto-languajes-vercel-sheysonn44s-projects.vercel.app", // tu frontend en Vercel
+];
 
-app.use(cors({
+const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: false,
-}));
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+};
+app.options("*", cors(corsOptions)); // Responde a preflight
 
+app.use(cors(corsOptions));
 // Ruta base para utilizar el servicio
 app.get("/api", function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
