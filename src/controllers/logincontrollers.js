398289bb484173977fac7d58711d2b361007be12
Sheysonn44 <Sheysonn44@gmail.com>
2025-07-03 15:13:21 -0600

    act

--- src/controllers/logincontrollers.js
+++ src/controllers/logincontrollers.js
@@ -4,21 +4,25 @@ const prisma = new PrismaClient();
 const { generateToken } = require('../../auth');
 
 exports.login = async (req, res) => {
-  const { id, password } = req.body;
+  const { usuario, password } = req.body;
 
   try {
     const teacher = await prisma.teacher.findMany({
-      where: { id: parseInt(id) },
+      where: {email: usuario  },
     });
 
     if (!teacher || teacher[0].password !== password) {
       return res.status(401).json({ message: 'Credenciales inválidas' });
     }
-    console.log('Holaaaaa');
+    console.log('ID:', id);
+    console.log('Password:', password
+    );
+  
     const token = generateToken(1);
     console.log(token)
     res.json({ token });
   } catch (error) {
+    console.error('Error al iniciar sesión:', error);
     res.status(500).json({ error: error });
   }
 };
