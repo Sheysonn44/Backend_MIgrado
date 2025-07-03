686800535d148f6c09a51ca061acf9ac70f987f5
Sheysonn44 <Sheysonn44@gmail.com>
2025-07-03 14:37:03 -0600

    inde

--- api/index.js
+++ api/index.js
@@ -10,7 +10,9 @@ const teacher = require("../src/routes/teacher.routes")
 
 
 //const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175'];
-const allowedOrigins = ['https://proyecto-languajes-vercel-sheysonn44s-projects.vercel.app', 'http://localhost:5173', ];
+const allowedOrigins = ['https://proyecto-languajes-vercel-sheysonn44s-projects.vercel.app', 'http://localhost:5173', 
+           'https://proyecto-languajes-vercel.vercel.app/'
+];
 
 app.use(cors({
   origin: function(origin, callback) {
