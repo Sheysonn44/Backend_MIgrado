5d9c91603c3d1346893bbb1d86610cd362418254
Sheysonn44 <Sheysonn44@gmail.com>
2025-07-03 12:45:35 -0600

    inde

--- api/index.js
+++ api/index.js
@@ -9,7 +9,21 @@ const login = require("../src/routes/loginrouters")
 const teacher = require("../src/routes/teacher.routes")
 
 
-app.use(cors());
+//const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175'];
+const allowedOrigins = ['https://proyecto-languajes-vercel.vercel.app', 'http://localhost:5173', ];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 // Ruta base para utilizar el servicio
 app.get("/api", function(req, res) {
@@ -20,6 +34,6 @@ app.get("/api", function(req, res) {
 app.use("/api/login", login);
 app.use("/api/teacher", teacher);
 
-
+app.get("/", (req, res) => res.send("Hola desde Vercel"));
 
 module.exports = app;
