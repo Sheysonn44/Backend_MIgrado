54f7b67bace5a808227f6c9ba924898a1b21a429
Sheysonn44 <Sheysonn44@gmail.com>
2025-07-03 14:26:50 -0600

    Ruta

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const teacher = require("../src/routes/teacher.routes")
 
 
 //const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175'];
-const allowedOrigins = ['https://proyecto-languajes-vercel.vercel.app', 'http://localhost:5173', ];
+const allowedOrigins = ['https://proyecto-languajes-vercel-sheysonn44s-projects.vercel.app', 'http://localhost:5173', ];
 
 app.use(cors({
   origin: function(origin, callback) {
